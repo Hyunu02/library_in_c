267fdbdd0456468fbe8a637bac75a24366191a4e
Hyunu02 <andrehwkc@gmail.com>
2021-11-16 23:14:16 -0300

    uepa

--- trainingA.c
+++ trainingA.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include "library1.h"
 
 int main () {
+    int a, b;
+    printf("Write two integer numbers\n");
+    scanf("%d %d", &a, &b);
+    printf("First number is: %d\n", a);
+    printf("Second number is: %d\n", b);
+
+    printf("Sum is: %d\n", soma (a, b));
+    printf("Subtraction is: %d\n", subtrai (a, b));
+    printf("Multiplication is: %d\n", multiplica (a, b));
     return 1;
 }
